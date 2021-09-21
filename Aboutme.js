d6aa274896e26ea163d779d4427634bf82b4718a
Bcracked <91127708+Bcracked@users.noreply.github.com>
2021-09-21 14:23:07 +0300

    About Me :)
    Information About Me, My Social Media Accounts And My Hobbies.

---
+++ Aboutme.js
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @Bcracked </>
+
+- 👀 I`m HTML & JavaScript & PHP & Swift & SQL & C# & C++ & Python - Developer.
+- 🌱 I'm Graphic Vfx And Vector Designer.
+
+- 💞️ (My Social Media:)
+
+- 'Discord: Bcracked#1882'
+- 'İnstagram: Bcracked_0006'
+- 'Behance: Be.net/spookys'
